591e62a6b8daf4fbd7a5e36fd8f44e2936fe1f25
duchiru <ngvit31082007@gmail.com>
2026-01-14 07:59:20 +0700

    feat: implement login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,51 @@
+"use client";
+
+import * as React from "react";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+import { InfoIcon } from "lucide-react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [id, setId] = React.useState("");
+
+  return (
+    <div className="w-dvw h-dvh flex items-center justify-center">
+      <div className="flex flex-col items-center gap-8">
+        <div>
+          <Alert>
+            <InfoIcon />
+            <AlertTitle>Đăng nhập bằng cách nhập mã định danh</AlertTitle>
+          </Alert>
+        </div>
+
+        <InputOTP maxLength={8} pattern={REGEXP_ONLY_DIGITS} value={id} onChange={setId}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+            <InputOTPSlot index={6} />
+            <InputOTPSlot index={7} />
+          </InputOTPGroup>
+        </InputOTP>
+
+        <Button>Đăng nhập</Button>
+      </div>
+    </div>
+  );
 }
